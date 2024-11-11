3202d667491e7ec8bf505d6d5707d350abeff9cc
humeniucDan <humeniuc.dan.stefan@gmail.com>
2024-11-11 10:47:58 +0200

    working jwt logic

--- userauth/crow/routes.cpp
+++ userauth/crow/routes.cpp
@@ -5,7 +5,7 @@
 #include "routes.h"
 #include "../login/login.h"
 #include "../mongodb/UserAuthRepo/userauthRepo.h"
-#include "../jsonwebtoken/jwtlogic/jwtlogic.h
+#include "../jsonwebtoken/jwtlogic/jwtlogic.h"
 
 void startRoutes(crow::App<crow::CookieParser, crow::SessionMiddleware<crow::FileStore>> &app){
     CROW_ROUTE(app, "/").methods("GET"_method)
@@ -27,8 +27,10 @@ void startRoutes(crow::App<crow::CookieParser, crow::SessionMiddleware<crow::Fil
 //                            return crow::response(400, "Password incorect");
 //                    }
 //                }
+                crow::response rsp(200, "jwtToken");
+                rsp.add_header("Set-Cookie", "generatedToken="+genToken(req.body));
 
-                return crow::response(200, ge);
+                return rsp;
             });
 
     CROW_ROUTE(app, "/signup").methods("POST"_method)
